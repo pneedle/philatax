aaea168bb54452ac9d2b75ea7c86b6af0aab1cf2
Peter Needle <pneedle@gmail.com>
2020-01-08 12:21:08 -0500

    tested preprocess function, added logo

--- themes/custom/philatax/js/philatax.js
+++ themes/custom/philatax/js/philatax.js
@@ -1,36 +1,14 @@
-/*
-$(function(){
-    'use strict';
-    var d = new Date();
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    
-    var year = d.getFullYear();
-    //console.log(d.getFullYear());
+// genersaate date formatted as: "Tuesday, January 7, 2020"
+jQuery(function(){
+  'use strict';
+  var d = new Date();
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    var month = months[d.getMonth()];
-    //console.log(months[d.getMonth()]);
+  var day = days[d.getDay()];
+  var month = months[d.getMonth()];
+  var date = d.getDate();
+  var year = d.getFullYear();
 
-    var date = d.getDate();
-    //console.log(d.getDate());
-
-    console.log(month + ' ' + date + ', ' + year);
+  console.log(day + ' ' + month + ' ' + date + ', ' + year);
 });
-*/
-var d = new Date();
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-   
-    var year = d.getFullYear();
-    //console.log(d.getFullYear());
-
-    var month = months[d.getMonth()];
-    //console.log(months[d.getMonth()]);
-
-    var date = d.getDate();
-    //console.log(d.getDate());
-
-    var day = days[d.getDay()];
-    //console.log(d.getDate());
-
-    console.log(day + ' ' + month + ' ' + date + ', ' + year);
-console.log('test1-2-3');
